ccb306516b88c618ef391dbf3ebc0c2800e4b0a5
Omri Bahumi <omrib@everything.me>
2014-03-31 23:08:49 +0300

    Added OSX support to the arp module

--- test/test_arp.py
+++ test/test_arp.py
@@ -5,28 +5,47 @@ import mock
 
 class ArpTestCase(unittest.TestCase):
     def setUp(self):
-        self.data = ('IP address       HW type     Flags       HW address            Mask     Device\n' +
-                     '192.168.0.94     0x1         0x2         aa:aa:aa:aa:aa:aa     *        br0\n' +
-                     '192.168.0.92     0x1         0x2         bb:bb:bb:bb:bb:bb     *        br0\n' +
-                     '192.168.0.200    0x1         0x2         cc:cc:cc:cc:cc:cc     *        br0\n' +
-                     '192.168.0.254    0x1         0x2         ee:ee:ee:ee:ee:ee     *        br0\n')
+        self.linux_data = ('IP address       HW type     Flags       HW address            Mask     Device\n'
+                           '192.168.0.94     0x1         0x2         aa:aa:aa:aa:aa:aa     *        br0\n'
+                           '192.168.0.92     0x1         0x2         bb:bb:bb:bb:bb:bb     *        br0\n'
+                           '192.168.0.200    0x1         0x2         cc:cc:cc:cc:cc:cc     *        br0\n'
+                           '192.168.0.254    0x1         0x2         ee:ee:ee:ee:ee:ee     *        br0\n')
 
-    def test_get_arp_table(self):
-        with mock.patch('__builtin__.open', mock.mock_open(read_data=self.data)):
+        self.darwin_data = ('? (192.168.0.1) at aa:aa:aa:aa:aa:aa on en0 ifscope [ethernet]\n'
+                            '? (192.168.0.230) at cc:cc:cc:cc:cc:cc on en0 ifscope [ethernet]\n'
+                            '? (192.168.0.250) at bb:bb:bb:bb:bb:bb on en0 ifscope [ethernet]\n')
+
+    def test_get_arp_table_linux(self):
+        with mock.patch('__builtin__.open', mock.mock_open(read_data=self.linux_data)):
             self.assertDictEqual(
-                utils.arp.get_arp_table(),
-                {'192.168.0.94':
-                     {'hw_address': 'aa:aa:aa:aa:aa:aa', 'flags': '0x2', 'device': 'br0', 'type': '0x1', 'mask': '*'},
-                 '192.168.0.200':
-                     {'hw_address': 'cc:cc:cc:cc:cc:cc', 'flags': '0x2', 'device': 'br0', 'type': '0x1', 'mask': '*'},
-                 '192.168.0.92': {
-                     'hw_address': 'bb:bb:bb:bb:bb:bb', 'flags': '0x2', 'device': 'br0', 'type': '0x1', 'mask': '*'},
-                 '192.168.0.254': {
-                     'hw_address': 'ee:ee:ee:ee:ee:ee', 'flags': '0x2', 'device': 'br0', 'type': '0x1', 'mask': '*'}
-                }
+                utils.arp.get_arp_table_linux(),
+                {'192.168.0.94': 'aa:aa:aa:aa:aa:aa',
+                 '192.168.0.200': 'cc:cc:cc:cc:cc:cc',
+                 '192.168.0.92': 'bb:bb:bb:bb:bb:bb',
+                 '192.168.0.254': 'ee:ee:ee:ee:ee:ee'}
             )
 
-    def test_get_mac_address(self):
-        with mock.patch('__builtin__.open', mock.mock_open(read_data=self.data)):
-            self.assertEqual(utils.arp.get_mac_address('192.168.0.94'), 'aa:aa:aa:aa:aa:aa')
+    def test_get_arp_table_darwin(self):
+        with mock.patch('subprocess.check_output', return_value=self.darwin_data):
+            self.assertDictEqual(
+                utils.arp.get_arp_table_darwin(),
+                {'192.168.0.230': 'cc:cc:cc:cc:cc:cc',
+                 '192.168.0.250': 'bb:bb:bb:bb:bb:bb',
+                 '192.168.0.1': 'aa:aa:aa:aa:aa:aa'}
+            )
+
+    def test_get_mac_address_linux(self):
+        with mock.patch('__builtin__.open', mock.mock_open(read_data=self.linux_data)):
+            for linux_platform in ('linux', 'linux2'):
+                with mock.patch('sys.platform', new=linux_platform):
+                    self.assertEqual(utils.arp.get_mac_address('192.168.0.94'), 'aa:aa:aa:aa:aa:aa')
+                    self.assertEqual(utils.arp.get_mac_address('1.1.1.1'), None)
+
+    def test_get_mac_address_darwin(self):
+        with mock.patch('sys.platform', new='darwin'), mock.patch('subprocess.check_output', return_value=self.darwin_data):
+            self.assertEqual(utils.arp.get_mac_address('192.168.0.250'), 'bb:bb:bb:bb:bb:bb')
             self.assertEqual(utils.arp.get_mac_address('1.1.1.1'), None)
+
+    def test_get_mac_address_unsupported(self):
+        with mock.patch('sys.platform', new='unsupported'):
+            self.assertRaises(Exception, utils.arp.get_mac_address, '1.1.1.1')
--- utils/arp.py
+++ utils/arp.py
@@ -1,4 +1,39 @@
+import sys
 import re
+import subprocess
+
+
+def get_arp_table_darwin():
+    """
+    Parse the host's ARP table on an OSX machine
+
+    :return: Machine readable ARP table (by running the "arp -a -n" command)
+    :rtype: dict {'ip_address': 'mac_address'}
+    """
+
+    arp_data_re = re.compile(r'^\S+ \((?P<ip_address>[^\)]+)\) at (?P<hw_address>(?:[0-9a-f]{2}:){5}(?:[0-9a-f]{2})) on (?P<device>\S+) ifscope \[(?P<type>\S+)\]$')
+
+    arp_data_raw = subprocess.check_output(['arp', '-a', '-n']).split("\n")[:-1]
+    parsed_arp_table = (arp_data_re.match(i).groupdict() for i in arp_data_raw)
+
+    return {d['ip_address']: d['hw_address'] for d in parsed_arp_table}
+
+
+def get_arp_table_linux():
+    """
+    Parse the host's ARP table on a Linux machine
+
+    :return: Machine readable ARP table (see the Linux Kernel documentation on /proc/net/arp for more information)
+    :rtype: dict {'ip_address': 'mac_address'}
+    """
+
+    with open('/proc/net/arp') as proc_net_arp:
+        arp_data_raw = proc_net_arp.read(-1).split("\n")[1:-1]
+
+    parsed_arp_table = (dict(zip(('ip_address', 'type', 'flags', 'hw_address', 'mask', 'device'), v))
+                        for v in (re.split('\s+', i) for i in arp_data_raw))
+
+    return {d['ip_address']: d['hw_address'] for d in parsed_arp_table}
 
 
 def get_arp_table():
@@ -6,18 +41,17 @@ def get_arp_table():
     Parse the host's ARP table
 
     :return: Machine readable ARP table (see the Linux Kernel documentation on /proc/net/arp for more information)
-    :rtype: dict {'ip_address':
-                    {'type': '', 'flags': '', 'hw_address': '<mac_address>', 'mask': '', 'device': '<nic>'},
-                    ...
-                }
+    :rtype: dict {'ip_address': 'mac_address'}
     """
 
-    # TODO: add support for more operating systems other than Linux
-    with open('/proc/net/arp') as proc_net_arp:
-        arp_data_raw = proc_net_arp.read(-1).split("\n")
+    if sys.platform in ('linux', 'linux2'):
+        return get_arp_table_linux()
+    elif sys.platform == 'darwin':
+        return get_arp_table_darwin()
+    else:
+        raise Exception("Unable to fetch ARP table on %s" % (sys.platform,))
 
-    return {v[0]: dict(zip(('type', 'flags', 'hw_address', 'mask', 'device'), v[1:]))
-            for v in (re.split('\s+', i) for i in arp_data_raw[1:-1])}
+
 
 
 def get_mac_address(ip):
@@ -31,7 +65,8 @@ def get_mac_address(ip):
     """
 
     arp_table = get_arp_table()
+
     if not ip in arp_table:
         return None
     else:
-        return arp_table[ip]['hw_address']
+        return arp_table[ip]
