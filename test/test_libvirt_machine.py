31eb67de473bffcd87de372bd2b8372d5b174bc2
Omri Bahumi <omrib@everything.me>
2014-12-01 11:20:14 +0200

    Improved user data test

--- test/test_libvirt_machine.py
+++ test/test_libvirt_machine.py
@@ -17,7 +17,7 @@ class LibvirtMachineTestCase(unittest.TestCase):
         self.assertEqual(self.machine.get_local_ipv4(), '192.168.0.1')
 
     def test_userdata(self):
-        self.assertEqual(self.machine.get_userdata(), """#cloud-config\ndisable_root: False\nssh_pwauth: False\nmanage_etc_hosts: False""")
+        self.assertEqual(self.machine.get_userdata(), """#cloud-config\ndisable_root: False\nssh_pwauth: False\nmanage_etc_hosts: False\nmultiple:\n  indentation:\n    levels: are cool""")
 
     def test_keys(self):
         keys = self.machine.get_keys()
